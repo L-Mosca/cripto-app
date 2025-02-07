8a54636fa477acea3fcaa47542053307f0af196e
Lucas Mosca <lucas.mosca@madeinweb.com.br>
2025-02-07 16:56:12 -0300

    Change app icon, name, status and navigation bar color.

--- app/src/main/java/com/example/cripto_app/presentation/list/ListScreen.kt
+++ app/src/main/java/com/example/cripto_app/presentation/list/ListScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,7 +25,7 @@ fun ListScreen(navController: NavHostController, viewModel: ListViewModel = hilt
     val isLoading = viewModel.isLoading.value
     val coinList = viewModel.coinList.value
 
-    Scaffold { innerPadding ->
+    Scaffold(containerColor = colorResource(R.color.dark_blue_600)){ innerPadding ->
         Box(
             modifier = Modifier
                 .padding(innerPadding)
